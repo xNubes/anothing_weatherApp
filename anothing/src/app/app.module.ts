0cbe0f6f4252bed74fcea642f57e6f5c60810c8e
Robin Jünemann <robin@juenemanns.de>
2023-12-12 14:23:06 +0100

    added http module

--- anothing/src/app/app.module.ts
+++ anothing/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ApiaccuService} from './apiaccu.service';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -9,8 +12,17 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    ApiaccuService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
